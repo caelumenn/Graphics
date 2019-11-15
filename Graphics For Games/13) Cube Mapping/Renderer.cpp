2cc2decc3f8e2e02e2a967c9b6c238e849b9693c
Shaw Ying <destinydy1213@gmail.com>
2019-11-15 15:28:00 +0000

    fix T15

--- Graphics For Games/13) Cube Mapping/Renderer.cpp
+++ Graphics For Games/13) Cube Mapping/Renderer.cpp
@@ -14,14 +14,25 @@ Renderer::Renderer(Window& parent) : OGLRenderer(parent) {
 	if (!reflectShader->LinkProgram() || !lightShader->LinkProgram() || !skyboxShader->LinkProgram()) {
 		return;
 	}
-	quad->SetTexture(SOIL_load_OGL_texture(TEXTUREDIR "water.TGA", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS));
+	quad->SetTexture(
+		SOIL_load_OGL_texture(
+			TEXTUREDIR "water.TGA",
+			SOIL_LOAD_AUTO,
+			SOIL_CREATE_NEW_ID,
+			SOIL_FLAG_MIPMAPS)
+	);
 
 	heightMap->SetTexture(SOIL_load_OGL_texture(TEXTUREDIR"Barren Reds.JPG", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS));
 
 	heightMap->SetBumpMap(SOIL_load_OGL_texture(TEXTUREDIR"Barren RedsDOT3.JPG", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS));
 
-	cubeMap = SOIL_load_OGL_cubemap(TEXTUREDIR"rusted_west.jpg", TEXTUREDIR"rusted_east.jpg", TEXTUREDIR "rusted_up.jpg", TEXTUREDIR"rusted_down.jpg",
-		TEXTUREDIR "rusted_south.jpg", TEXTUREDIR"rusted_north.jpg ", SOIL_LOAD_RGB, SOIL_CREATE_NEW_ID, 0);
+	cubeMap = SOIL_load_OGL_cubemap(
+		TEXTUREDIR"rusted_west.jpg", TEXTUREDIR"rusted_east.jpg",
+		TEXTUREDIR "rusted_up.jpg", TEXTUREDIR"rusted_down.jpg",
+		TEXTUREDIR "rusted_south.jpg", TEXTUREDIR"rusted_north.jpg",
+		SOIL_LOAD_RGB,
+		SOIL_CREATE_NEW_ID,
+		0);
 
 	if (!cubeMap || !quad->GetTexture() || !heightMap->GetTexture() || !heightMap->GetBumpMap()) {
 		return;
@@ -71,10 +82,10 @@ void Renderer::RenderScene() {
 void Renderer::DrawSkybox() {
 	glDepthMask(GL_FALSE);
 	SetCurrentShader(skyboxShader);
-	
+
 	UpdateShaderMatrices();
-	quad -> Draw();
-	
+	quad->Draw();
+
 	glUseProgram(0);
 	glDepthMask(GL_TRUE);
 }
